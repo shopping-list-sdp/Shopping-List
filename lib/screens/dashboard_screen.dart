36cfe331d5347923e6c78a26e3c285582a6f1094
laylaa <laylaavarachia@gmail.com>
2022-10-11 11:43:37 +0200

    removed-concept-list

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -38,7 +38,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
                   //allow page to scroll
                   child: Padding(
                 padding: EdgeInsets.fromLTRB(
-                    35, MediaQuery.of(context).size.height * 0.05, 35, 0),
+                    35, MediaQuery.of(context).size.height * 0.1, 35, 0),
                 child: Column(children: <Widget>[
                   Text("Home", //title of page
                       style: TextStyle(
@@ -51,15 +51,15 @@ class _DashboardScreenState extends State<DashboardScreen> {
                   Row(mainAxisAlignment: MainAxisAlignment.center, children: [
                     dashboardButtons(context, "Blue", "My List",
                         const MyListScreen()), //my list screen
-                    dashboardButtons(context, "Red", "Family List",
+                    dashboardButtons(context, "Pink", "Family List",
                         const MyListScreen()), //takes you to family list
                   ]),
                   const SizedBox(
                     height: 30,
                   ),
                   Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                    dashboardButtons(context, "Pink", "Concept List",
-                        const MyListScreen()), //takes you to concept list
+                    dashboardButtons(
+                        context, "Yellow", "Scheduled", const MyListScreen()),
                     dashboardButtons(
                         context,
                         "Purple",
@@ -73,8 +73,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
                   Row(mainAxisAlignment: MainAxisAlignment.center, children: [
                     dashboardButtons(context, "Green", "Expenses",
                         const MyListScreen()), //take you to spending page
-                    dashboardButtons(context, "Yellow", "Scheduled",
-                        const MyListScreen()), //take you to scheduled page
+                    //take you to scheduled page
                   ]),
                 ]),
               )),

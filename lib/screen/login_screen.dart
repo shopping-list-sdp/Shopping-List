3f178dbf73e7125a21762dd1abb5571c98f8814d
abdullahlaher <laherabdullah5@gmail.com>
2022-10-11 15:34:07 +0200

    testing updated

---
+++ lib/screen/login_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({
+    super.key,
+    required this.title,
+    required this.message,
+  });
+
+  final String title;
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+          child: Text(message),
+        ),
+      ),
+    );
+  }
+}
+
+class AssetsForScreen {
+  int value = 0;
+
+  void increment() => value++;
+
+  void decrement() => value--;
+}
